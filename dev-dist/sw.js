b3eeae52a03c2a8ff8bb5f5669ed61a9c73b3d13
kkc <bushwick97@naver.com>
2025-06-04 11:18:03 +0900

    Initial commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.avfubl1gijg"
+    "revision": "0.6u80ijsgsjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -9,31 +9,40 @@ import shareIcon from "../assets/img/main/icons/share_icon.png";
 import saveIcon from "../assets/img/main/icons/save_icon.png";
 
 const VideoItem = ({videoData}) => {
+    //video 태그를 직접 조작하기위해  useRef() 사용
     const videoRef = useRef(null);
     const [isCommentOpen, setIsCommentOpen] = useState(false);
     const [comments, setComments] = useState([...videoData.comments]);
 
     // IntersectionObserver 생성 후 비디오 요소 관찰
     useEffect(() => {
+        //현재 비디오 요소
         const videoEl = videoRef.current;
+        //비디오 요소 없으면 종료
         if (!videoEl) return;
 
+        //DOM 요소가 다른 요소(주로 뷰포트)와 교차하는 정도를 비동기적으로 감지하는 API.
         const observer = new IntersectionObserver(
+                //entries : 감지된 대상들에 대한 정보가 배열들로 들어옴
                 (entries) => {
+                    //entries는 관찰중인 DOM 요소들의 상태가 담긴 객체들이고, entry는 그 중 하나.
                     entries.forEach((entry) => {
-                        if (entry.isIntersecting) {
-                            entry.target.play();
-                            entry.target.style.opacity = "1";
+                        //반복문으로 하나씩 상태를 꺼내서 검사함.
+                        if (entry.isIntersecting) { //entry.isIntersecting은 해당 요소가 화면에 지정된 비율만큼 보이면 true가 됨.
+                            entry.target.play(); //entry.target : 관찰중인 요소 , play() 재생
+                            entry.target.style.opacity = "1"; // 투명도 100%
                         } else {
-                            entry.target.pause();
-                            entry.target.style.opacity = "0.5";
+                            entry.target.pause(); //일시정지
+                            entry.target.style.opacity = "0.5"; //투명도 50%
                         }
                     });
                 },
-                {threshold: 0.6}
+                {threshold: 0.6} //60%가 보이면 observer 실행
         );
 
+        //무슨 대상을 관찰할지
         observer.observe(videoEl);
+        //메모리를 위해 클린업
         return () => observer.unobserve(videoEl);
     }, []);
 
@@ -44,19 +53,22 @@ const VideoItem = ({videoData}) => {
 
         const onLoadedMetadata = () => {
             if (videoEl.videoWidth <= videoEl.videoHeight) {
-                videoEl.classList.add("heightVideo");
+                videoEl.classList.add("heightVideo"); //세로영상
             } else {
-                videoEl.classList.add("widthVideo");
+                videoEl.classList.add("widthVideo"); //가로영상
             }
         };
+        //메타데이터 로드시 함수실행
         videoEl.addEventListener("loadedmetadata", onLoadedMetadata);
+        //클린업
         return () => videoEl.removeEventListener("loadedmetadata", onLoadedMetadata);
     }, []);
 
     // 댓글 토글 클릭 핸들러
     const toggleComment = () => {
         setIsCommentOpen((prev) => !prev);
-        if (window.innerWidth <= 768) {
+
+        if (window.innerWidth <= 768) { //모바일 기기일경우
             // 모바일에서 댓글창 열릴 때 비디오 일시정지, 닫힐 때 다시 재생
             const videoEl = videoRef.current;
             if (isCommentOpen) {
