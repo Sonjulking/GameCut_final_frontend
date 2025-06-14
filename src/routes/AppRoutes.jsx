d6360d7a8f7ad119ef3b13b57695cbe576056d93
jeonhj1015 <mumu097@naver.com>
2025-06-14 21:32:56 +0900

    [feat] 게시글 목록 구현

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,41 +1,30 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "../layout/Layout.jsx";
-import React, {lazy, Suspense} from "react";
+import React, { lazy, Suspense } from "react";
 import HomePage from "../pages/HomePage.jsx";
 import BoardWrite from "../pages/BoardWrite.jsx";
 import LoadingScreen from "../components/Loading/LoadingScreen.jsx";
-
+import BoardList from "../pages/BoardList.jsx";
 // ⏱️ 5초 delay wrapper
 const delayImport = (importFunc, delay = 5000) => {
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(importFunc()), delay);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(importFunc()), delay);
+  });
 };
 
 const TestPage = lazy(() => delayImport(() => import("../pages/Test.jsx")));
 
 const AppRoutes = () => {
-    return (
-            <Routes>
-                <Route
-                        path="/"
-                        element={<Layout/>}
-                >
-                    <Route
-                            index
-                            element={<HomePage/>}
-                    />
-                    <Route
-                            path="/test"
-                            element={<TestPage/>}
-                    />
-                    <Route
-                            path="/boardWrite"
-                            element={<BoardWrite/>}
-                    />
-                </Route>
-            </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/boardWrite" element={<BoardWrite />} />
+        <Route path="/boardList" element={<BoardList />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default AppRoutes;
