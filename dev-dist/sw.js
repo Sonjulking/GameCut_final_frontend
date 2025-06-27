de9a921d81348439b8b1181f59aa5db7c0cc39d5
woojinkong <dodejqn6@gmail.com>
2025-06-27 09:49:45 +0900

    <feat>  sw fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lam2uck3kcg"
+    "revision": "0.089hftptrp8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
