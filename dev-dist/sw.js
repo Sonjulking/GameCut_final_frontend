47826973f8fedb299a589c3f978c2bb2a096a968
woojinkong <dodejqn6@gmail.com>
2025-07-01 10:40:24 +0900

    <feat> 마이페이지 내댓글 사이드바 비활성문제 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ekohamijp08"
+    "revision": "0.hd1hu4npgko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
