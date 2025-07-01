509658fb1e4477b4cdcf388fa5681f5c7b4143ad
jeonhj1015 <mumu097@naver.com>
2025-07-01 21:04:09 +0900

    <fix>토큰, 로그인 유저 상태유지 기능

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -4,22 +4,29 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   isLoggedIn: false,
   user: null, // 전체 유저 정보 저장
+  isAuthChecking: false,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    startAuthCheck(state) {
+      state.isAuthChecking = true;
+    },
     loginSuccess(state, action) {
       state.isLoggedIn = true;
       state.user = action.payload; // ex: { userId, userName, nickname, email, ... }
+      state.isAuthChecking = false; // ✅ 인증 확인 완료
     },
+
     logout(state) {
       state.isLoggedIn = false;
       state.user = null;
+      state.isAuthChecking = false; // ✅ 인증 확인 완료
     },
   },
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const { loginSuccess, logout, startAuthCheck } = authSlice.actions;
 export default authSlice.reducer;
